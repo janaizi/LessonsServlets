0963805892ddf2adf9d9f8ccc9a20abbcc7b3915
jsaliev <jsaliev@MBRJSALIEV.molbulak.ru>
2022-05-18 16:16:03 +0300

    Finish lesson - 2
    JSTL,taglib
    Servlet JSP
    set and ged attributes

--- src/main/java/org/jas/LessonsServlets/servlets/GetIndexPageServlet.java
+++ src/main/java/org/jas/LessonsServlets/servlets/GetIndexPageServlet.java
@@ -1,38 +1,76 @@
 package org.jas.LessonsServlets.servlets;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jas.LessonsServlets.model.User;
+
 /**
  * Servlet implementation class GetIndexPageServlet
  */
 public class GetIndexPageServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private final static String index = "/WEB-INF/view/index.jsp";
 
-    public GetIndexPageServlet() {
-        // TODO Auto-generated constructor stub
-    }
-    
-    private static String index = "/WEB-INF/view/index.jsp";
-    
-    @Override
-    public void init() throws ServletException {
-        System.out.println("*************SERVLET IS INIT************");
-        System.out.println("FOR PATH '/' WILL RENDER VIEW : " + index);
-    }
-    
-    //Multithreading scope.
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public GetIndexPageServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private List<User> users;
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("*************SERVLET IS INIT************");
+		System.out.println("FOR PATH '/' WILL RENDER VIEW : " + index);
+		users = new CopyOnWriteArrayList<User>();
+		users.add(new User("Janysh", 26));
+		users.add(new User("Baiysh", 26));
+	}
+
+	// Multithreading scope.
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("doGet is work!");
+		request.setAttribute("users", users);
 		request.getRequestDispatcher(index).forward(request, response);
 	}
-	
+
 	@Override
-    public void destroy() {
-        System.out.println("*************SERVLET IS DESTROY************");
-    }
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doPost is work!");
+		
+		req.setCharacterEncoding("UTF8");
+		
+		if (!requestIsValid(req)) {
+			doGet(req, resp);
+		}
+
+		final String name = req.getParameter("name");
+		final String age = req.getParameter("age");
+
+		final User user = new User(name, Integer.valueOf(age));
+
+		users.add(user);
+
+		doGet(req, resp);
+	}
+
+	private boolean requestIsValid(final HttpServletRequest req) {
+
+		final String name = req.getParameter("name");
+		final String age = req.getParameter("age");
+
+		return name != null && name.length() > 0 && age != null && age.length() > 0 && age.matches("[+]?\\d+");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("*************SERVLET IS DESTROY************");
+	}
 
 }
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -1,5 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jstl/core" prefix="c"%>
+<!DOCTYPE html>
 <html>
+<head>
+<meta charset="utf-8">
+<title>INDEX</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+	<h2>Все пользователи</h2>
+	<br />
+	<ul>
+		<c:forEach var="user" items="${requestScope.users}">
+				Имя: <c:out value="${user.name}" />
+			<br />
+				Возраст: <c:out value="${user.age}" />
+			<br />
+		</c:forEach>
+	</ul>
+
+	<h2>Создание нового пользователя</h2>
+	<br />
+
+	<form method="post" action="">
+		<label>Имя<input type="text" name="name"></label><br>
+		
+		<label>Возраст<input type="number" name="age"></label><br>
+		
+		<input type="submit" value="Ok" name="Ok"><br>
+	</form>
+
 </body>
 </html>
